89ce39070a18dabb7dea61783ddcf4008b9dc214
Raju5889 <thyagaraju.ptr@gmail.com>
2020-12-21 00:54:59 +0530

    added new button hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div>
       <h1>Thyagaraju is awesome</h1>
       <p>this is development with new feature</p>
+      <Button>Hello</Button>
     </div>
   );
 }
